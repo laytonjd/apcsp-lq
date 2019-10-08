c4f7def0db6d6715463a9e68460d1b4fcc1c0571
Jonah Layton <jlayton818@gmail.com>
2019-10-08 10:20:22 -0600

    three_even? doesn't know position of the even numbers so doesn't fully work, series_up is a work in progress

--- list_quiz.rb
+++ list_quiz.rb
@@ -3,7 +3,6 @@ def three_even?(list)
     num = 0
     list.each do |i|
         if i % 2 == 0
-            if
             count += 1
         end
     end
@@ -27,7 +26,7 @@ def bigger_two(list1, list2)
         list1.each do |num1|
             sum1 = sum1 + num1
         end
-        list2.each do |num2|
+            list2.each do |num2|
             sum2 = sum2 + num2
         end
         if sum2 > sum1
@@ -39,3 +38,26 @@ end
 print bigger_two([1, 2], [3, 4]) # [3, 4]
 
 print bigger_two([1, 7], [4, 4]) # [1, 7]
+
+def series_up(n)
+    count = 0
+    list = []
+    x = 1
+    n.times do |nums|
+        list.push (x)
+            n.times do |nums2|
+                
+            end
+
+        if n == 1
+            list = [1]
+        if n == 2
+            list = [1, 1, 2] 
+    end
+end
+
+
+puts series_up(1) #[1]
+puts series_up(2) #[1, 1, 2]
+puts series_up(3) #[1, 1, 2, 1, 2, 3]
+puts series_up(4) #[1, 1, 2, 1, 2, 3, 1, 2, 3, 4]
