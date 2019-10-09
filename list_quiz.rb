13a1aaac0dc155f067ecc5cd4db87a95c817874b
Jonah Layton <jlayton818@gmail.com>
2019-10-09 10:09:55 -0600

    Working and completed quiz loops

--- list_quiz.rb
+++ list_quiz.rb
@@ -1,15 +1,12 @@
 def three_even?(list)
-    count = 0
-    num = 0
-    list.each do |i|
-        if i % 2 == 0
-            count += 1
+    x = 0 # variable to describe the position of the list
+    (list.size - 1).times do |i| # use times instead of each because of the change in the list.size instead of list.each character
+        if list[x] % 2 == 0 && list[x + 1] % 2 == 0 && list[x - 1] % 2 == 0 # starts a position 0 in the list and checks position 0, 1, 2 at the start
+            return true
         end
+        x = x + 1 # change the x value so that the position changes by 1. It checks position 1, 2, 3 this time around 
     end
-    if count == 3
-        return true
-    end
-    return false
+    return false # if the first statement isn't true after the loop it returns false 
 end
 
 puts three_even?([2, 1, 3, 5]) # false
@@ -38,26 +35,25 @@ end
 print bigger_two([1, 2], [3, 4]) # [3, 4]
 
 print bigger_two([1, 7], [4, 4]) # [1, 7]
+puts "\n"
 
 def series_up(n)
-    count = 0
+    highest_num = 1
     list = []
-    x = 1
-    n.times do |nums|
-        list.push (x)
-            n.times do |nums2|
-                
-            end
-
-        if n == 1
-            list = [1]
-        if n == 2
-            list = [1, 1, 2] 
+    num = 1
+    n.times do # loop for the number of times to do the loop
+        num = 1 # resets the list to 1 so the series starts at 1
+           while num <= highest_num # prints the numbers from 1 to the number in the input value. Loop in a loop 
+                list.push(num) # pushes a number into the list everytime the loop is run
+                num = num + 1 # increase the number until it is bigger than the highest number
+           end
+           highest_num = highest_num + 1 # sets the highest number up by 1 so the series get longer every time through
     end
+    return list
 end
 
 
-puts series_up(1) #[1]
-puts series_up(2) #[1, 1, 2]
-puts series_up(3) #[1, 1, 2, 1, 2, 3]
-puts series_up(4) #[1, 1, 2, 1, 2, 3, 1, 2, 3, 4]
+print series_up(1) #[1]
+print series_up(2) #[1, 1, 2]
+print series_up(3) #[1, 1, 2, 1, 2, 3]
+print series_up(4) #[1, 1, 2, 1, 2, 3, 1, 2, 3, 4]
